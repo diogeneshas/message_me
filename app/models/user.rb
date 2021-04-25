2f8e9e0328339af3a8a1039da120f5ebfb2fa7d8
Diogenes <diogenes.henrique.br@gmail.com>
2021-04-25 02:24:47 -0300

    Build message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates :username,  presence: true, length: { mininum: 3, maximum: 15 }
     validates_uniqueness_of :username
+    has_many :messages
     has_secure_password
 end
